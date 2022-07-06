44a633049a74fed61c1aabc72e98fe6de3c5a695
dwyrwas <djwyrwas@gmail.com>
2022-07-06 14:55:23 -0500

    get for random quotes and quote by author

--- server.js
+++ server.js
@@ -8,4 +8,23 @@ const PORT = process.env.PORT || 4001;
 
 app.use(express.static('public'));
 
+app.get('/api/quotes/random', (req, res, next) => {
+    const randomQuote = { quote: getRandomElement(quotes)};
+    if(randomQuote){
+        res.status(200).send(randomQuote);
+    } else {
+        res.status(404).send();
+    }
+});
+
+app.get('/api/quotes', (req, res, next) => {
+    const reqPerson = req.query.name;
+    if(reqPerson){
+        const quotesByAuthor = quotes.filter(quote => quote.person === reqPerson);
+        res.send(quotesByAuthor);
+    } else {
+        res.send(quotes);
+    }
+})
+
 app.listen(PORT);
